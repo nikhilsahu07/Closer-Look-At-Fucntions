6af71f1d8def7237c7d03cdef175ebe8c69f19ca
Nikhil Sahu <heynikhil07@gmail.com>
2023-06-01 09:15:05 +0530

    Lecture: #2 Learned about Passing arguements working:Values vs. Reference

--- script.js
+++ script.js
@@ -29,3 +29,39 @@
 // //skiping parameters
 // listBooking('LH123', undefined, 12000);
 
+
+// /////////////////// How Passing Arguements Works: Values vs Reference //////////////////
+
+// const flight = 'LH234';
+// const nikhil = {
+//     hisName: 'Nikhil Sahu',
+//     gender: 'M',
+//     passportNum: 452345823272,
+// };
+
+// const checkIN = function (flightNum, passenger) {
+//     flightNum = 'LH289';
+//     passenger.hisName = `${passenger.gender == 'M' ? 'Mr.' : 'Mrs.'} ${passenger.hisName}`;
+
+//     if (passenger.passportNum === 452345823272) alert('Checked In')
+//     else alert('Wrong passport..!')
+
+// };
+
+// // checkIN(flight, nikhil);
+
+// // console.log(flight); //primitive value doesn't change, it just make a copy of that
+// // console.log(nikhil); //inside the function we changes the object data struct
+
+// // Is the same as doing
+// // const flightNum = flight;
+// // const passenger = nikhil;
+
+// const newPassport = function (person) {
+//     person.passportNum = Math.trunc(Math.random() * 1000000000);
+// }
+
+// newPassport(nikhil);
+// checkIN(flight, nikhil)
+// console.log(nikhil.passportNum);
+
