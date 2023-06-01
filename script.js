5b3349cefe4323298d799a23e893ef6ae7fe99b1
Nikhil Sahu <heynikhil07@gmail.com>
2023-06-01 07:07:08 +0530

    Lecture: #1 Learned about Defaulting Parameters

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 'use strict'
+
+// ///////////////////// Default Parameters ////////////////////////
+
+// const bookings = [];
+
+// const listBooking = function (flightNum,
+//     numPassengers = 1,
+//     price = numPassengers * 8000) {
+
+//     //ES5 of defaulting parameters
+//     // numPassengers = numPassengers || 1;
+//     // price = price || 8000;
+
+//     const booking = {
+//         flightNum,
+//         numPassengers,
+//         price,
+//     }
+//     console.log(booking);
+//     bookings.push(booking);
+
+// };
+
+// listBooking('LH123');
+// listBooking('LH231', 2, 16000);
+// listBooking('LH421', 4);
+
+// //skiping parameters
+// listBooking('LH123', undefined, 12000);
+
